d385b67e7b97525bc2b0214af6d6ab4af0fedd67
CNLPY <calpay@yahoo.com>
2022-02-11 14:08:22 +0300

    Initial Commit

---
+++ Bmi/BmiApp.swift
@@ -0,0 +1,17 @@
+//
+//  BmiApp.swift
+//  Bmi
+//
+//  Created by Can on 11.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct BmiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
