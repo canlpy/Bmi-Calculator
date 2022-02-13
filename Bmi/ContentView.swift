e7e65dd2a8096cb90632410152cb6da4c346c982
CNLPY <calpay@yahoo.com>
2022-02-13 17:54:37 +0300

    v0.2 Bmi calculation func working

--- Bmi/ContentView.swift
+++ Bmi/ContentView.swift
@@ -12,18 +12,18 @@ struct ContentView: View {
     @State private var sexuality = ""
     @State private var heightType = ""
     @State private var weightType = ""
-    @State private var height : Int
+    @State private var height : Double = 130.0
     @State private var weight = 60
     @State private var age = 30
-    @State private var result : Int
+    @State private var result : Double = 0.0
+    @State private var heightCm = 160.0
+    @State private var isEditing = false
     
     func calculate()  {
         
-        
-        height =
-        
-        result = 
-        
+    height = heightCm / 100
+        result = Double(weight) / (height * height)
+
         
     }
     
@@ -31,8 +31,7 @@ struct ContentView: View {
     
     
     
-    @State private var speed = 50.0
-    @State private var isEditing = false
+    
     
     var body: some View {
         ZStack(alignment: .bottom ) {
@@ -95,14 +94,13 @@ struct ContentView: View {
                 
                 VStack(alignment: .center) {
                     HStack{
-                        Text("Height")
-                        Text(String(speed))
-                        Text("Cm")
-                    }.foregroundColor(.white).font(.title)
+                       Text("Height \(Int(heightCm)) cm")
+                  }.foregroundColor(.white).font(.title)
                     
                     Slider(
-                        value: $speed,
-                            in: 120...230
+                        value: $heightCm,
+                            in: 120...230 ,
+                        step: 1
                         )
                        
                     
@@ -177,7 +175,9 @@ struct ContentView: View {
                     .foregroundColor(Color.white)
                     .frame(width: 332.0, height: 60.0)
                     .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.gray/*@END_MENU_TOKEN@*/)
-                Text("Your Bmi "  + String(result))
+                
+                Spacer()
+                Text("Your Bmi "  + String(format: "%.1f", result))
                 
                     
                 Spacer()
